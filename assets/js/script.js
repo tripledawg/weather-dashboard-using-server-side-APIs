b0b4ff954d4ef684492e8b7ac7347e73daa36d4b
tripledawg <iamlesliecrouch>
2021-12-03 21:19:37 -0500

    added global root weather api and api for getting lat and lon, fixed position of static placeholder elements for the elements that will be dynamically populated later

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 //var for current day formatted to 3/30/2021 look
 //local storage for persistent data
 //var for API call
-//var to store lat lon for other cities
 //search button function, whatever city load temp, wind, humidity and UV index to jumbotron and 5 day to footer
 //city button functions to  populate current temp wind humidity and UV index to the jumbotron and 5 day to the footer
 //UV index color coded based on severity (favorable, moderate, severe)
@@ -9,6 +8,11 @@
 //weather api key c26d4f483a9680cf07042618df1ef271
 
 //current city name  --get from variable name assigned to lat lon??
+//var city; 
+//api call search city api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//eg api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=c26d4f483a9680cf07042618df1ef271
+//eg api.openweathermap.org/data/2.5/find?q=Atlanta&appid={c26d4f483a9680cf07042618df1ef271
+//eg api.openweathermap.org/data/2.5/find?q={city}&appid={apid}
 //{current}dt for date  and use moment formatting??  current.dt
 //{current}/[weather]/icon: value  for picture  current.weather.id.??val??
 //{current}/temp current.temp 
@@ -22,8 +26,32 @@
 //{daily}/humidity  daily.humidity
 //{daily}/[weather]/icon: value  for picture  daily.weather.id.??val??
 
+//accept user input and store in city varaible
 
-var requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=28.08&lon=80.60&exclude=hourly,minutely,alerts&appid=c26d4f483a9680cf07042618df1ef271";
+var userSearchInput = ""; 
+var citySearched = "";
+var defaultCity = "Atlanta"
+var APIKey = "c26d4f483a9680cf07042618df1ef271";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var requestURLMelbourneFl = "https://api.openweathermap.org/data/2.5/onecall?lat=28.08&lon=80.60&exclude=hourly,minutely,alerts&appid=c26d4f483a9680cf07042618df1ef271";
+var rootWeatherAPI = "https://api.openweathermap.org"
+//https://openweathermap.org/api/geocoding-api#direct// 
+function generateLatLonFromCity () {
+
+    return city;
+};
+
+
+fetch (queryURL)
+.then(function (response) {
+    return response .json();
+})
+.then (function (data){
+    console.log("fetch respnse \n----------");
+    console.log(data); 
+}); 
+////
+
 
 fetch (requestURL)
 .then(function (response) {
@@ -32,7 +60,12 @@ fetch (requestURL)
 .then (function (data) {
     console.log("fetch response \n-----------");
     console.log(data);
+    var container = document.getElementById('day-1');
+    var dayOneEl=document.createElement('p');
+    for (var i = 0; i < data.length; i++) {
+    console.log(data[i].daily.weather.id);
+    dayOneEl.textContent=data[i].daily.weather.id;
+    container.append(dayOneEl);
+
+    }
 });
-
-
-
